2c20656189cbcf2b0f622c10219c1c077c37c9a4
Joel Mayer <joelmayer@Andys-Mac-Pro.local>
2013-09-04 17:02:59 +0100

    Fixed no avatar error and added new friendship methods

--- app/controllers/user_friendships_controller.rb
+++ app/controllers/user_friendships_controller.rb
@@ -4,6 +4,16 @@ class UserFriendshipsController < ApplicationController
     @user_friendships = current_user.user_friendships.all
   end
   
+  def accept
+    @user_friendship = current_user.user_friendships.find(params[:id])
+    if @user_friendship.accept!
+      flash[:notice] = "You are now friends with #{@user_friendship.friend.full_name}"
+    else
+      flash[:alert] = "That friendship was not accepted"
+    end
+    redirect_to user_friendships_path
+  end
+  
   def new
     if params[:friend_id]
       @friend = User.where(profile_name: params[:friend_id]).first
@@ -21,11 +31,16 @@ class UserFriendshipsController < ApplicationController
       @friend = User.where(profile_name: params[:user_friendship][:friend_id]).first
       @user_friendship = current_user.user_friendships.new(friend: @friend)
       @user_friendship.save
-      flash[:notice] = "You are now friends"
+      flash[:notice] = "You are now friends with #{@friend.full_name}"
       redirect_to profile_path(@friend)
     else
       flash[:alert] = "Friend required"
       redirect_to root_path
     end
+  end
+  
+  def edit
+    @user_friendship = current_user.user_friendships.find(params[:id])
+    @friend = @user_friendship.friend    
   end
 end
